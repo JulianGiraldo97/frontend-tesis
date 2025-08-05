45e8e19d615f5f159cbd8c3020cafadf42b7a50b
Julian Giraldo <julian09426@gmail.com>
2025-08-05 16:06:54 -0500

    Agregar basename al BrowserRouter para compatibilidad con GitHub Pages

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/frontend-tesis">
       <AccessibilityProvider>
         <AuthProvider>
           <App />
